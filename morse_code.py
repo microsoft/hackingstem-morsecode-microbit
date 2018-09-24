959e8bee6432a1e0d35338571b2d1b332268ec34
Jeremy Franklin-Ross <v-jerfra@microsoft.com>
2018-09-24 14:04:59 -0700

    cleaned up, comments

--- morse_code.py
+++ morse_code.py
@@ -1,69 +1,94 @@
+# ------------__ Hacking STEM – morse_code.py – micro:bit __-----------
+# For use with the Harnessing Electricity to Communicate Lesson plan
+# from Microsoft Education Workshop at http://aka.ms/hackingSTEM
+#
+#  This project uses a speaker across Pin 0 and GND as well as the 
+#  left built in button. Using short and long button presses, up to 
+#  20 dots, dashes, and spaces will be transmitted to Excel, which can
+#  be used to decode messages.
+#  
+#   * Short button hold will display a dot on LED grid and tone 
+#   * Long button hold will display a dash on LED grid and tone 
+#   * Extra long button hold will display an X on LED grid and no 
+#  character will transmitted to Excel
+#   * Pause between button presses will result in a space.
+#   * If all 20 slots are filled, button pressed will display an X on LED
+#  grid and no data will be transmitted to Excel
+#   * Serial messages sent from excel will display on LED matrix and make
+#  tones via speaker
+#
+#  This project uses a BBC micro:bit microcontroller, information at:
+#  https://microbit.org/
+#
+#  Comments, contributions, suggestions, bug reports, and feature
+#  requests are welcome! For source code and bug reports see:
+#  http://github.com/[TODO github path to Hacking STEM]
+#
+#  Copyright 2018, Jeremy Franklin-Ross
+#  Microsoft EDU Workshop - HackingSTEM
+#  MIT License terms detailed in LICENSE.txt
+# ===---------------------------------------------------------------===
 from microbit import * 
 import music
-
-mark_has_begun = False
-mark_start_time = running_time() 
-
-space_start_time = running_time() 
-space_has_begun = False
-
-message = list()
 
 CHARACTER_LIMIT = 20
 
 EOL = "\n"
 
-WORDS_PER_MINUTE = 10
-DOT_TIME = 1200/WORDS_PER_MINUTE
-DASH_TIME = DOT_TIME * 3
-MARK_SPACE = DOT_TIME
-WORD_SPACE = DOT_TIME * 7
+WORDS_PER_MINUTE = 10             # Scale for all durations
+DOT_TIME = 1200/WORDS_PER_MINUTE  # Duration of an ideal dot mark
+DASH_TIME = DOT_TIME * 3          # Duration of ideal dash mark
+MARK_SPACE = DOT_TIME             # Time between marks
+WORD_SPACE = DOT_TIME * 7         # Time between words
 
-DOT_TIME_MIN = DOT_TIME/4
-DOT_TIME_MAX = DASH_TIME/2
+""" Following thresholds are used to detect input """
+DOT_TIME_MIN = DOT_TIME/4         # Minimum dot duration
+DOT_TIME_MAX = DASH_TIME/2        # Maximum dot duration
 
-DASH_TIME_MIN = DOT_TIME_MAX
-DASH_TIME_MAX = DASH_TIME + (DOT_TIME * 2)
+DASH_TIME_MIN = DOT_TIME_MAX                # Min dash duration
+DASH_TIME_MAX = DASH_TIME + (DOT_TIME * 2)  # Max dash duration
 
-SIGNAL_SPACE_MAX = WORD_SPACE * 2
-WORD_SPACE_MIN = SIGNAL_SPACE_MAX
+SIGNAL_SPACE_MAX = WORD_SPACE * 2  # Max duration between letters
+WORD_SPACE_MIN = SIGNAL_SPACE_MAX  # Min duration between words
 
-DOT = "."
-DASH = "-"
+# Characters for dot, dash, space
+DOT = "."   
+DASH = "-"  
 SPACE = " "
 
-DOT_IMAGE = Image("00000:"
-                  "00000:"
-                  "00900:"
-                  "00000:"
-                  "00000")
-DASH_IMAGE = Image("00000:"
-                   "00000:"
-                   "99999:"
-                   "00000:"
-                   "00000")
-OVER_TIME_IMAGE = Image("90009:"
-                        "09090:"
-                        "00900:"
-                        "09090:"
-                        "90009")
+""" LED matrix images of dots, dashes, and over time """
+DOT_IMAGE = Image("00000:00000:00900:00000:00000")
+DASH_IMAGE = Image("00000:00000:99999:00000:00000")
+OVER_TIME_IMAGE = Image("90909:09090:00900:09090:90009")
 
-TONE_PITCH = 800
+TONE_PITCH = 800  # Frequency to play through speaker
+
+''' State and timers to derive button input '''
+mark_has_begun = False             # State when key pressed
+mark_start_time = running_time()   # Used to derive dot or dash
+space_has_begun = False            # State when key let up
+space_start_time = running_time()  # Used to derive space
+message = list()  # List accumulates Dots, Dashes, and Spaces 
+
 
 def not_at_max_message_length():
+    """ Indicates if we have room left in message """
     global message
     return len(message) < CHARACTER_LIMIT
 
 def clear_command_received():
-    #TODO
+    """ When clear button was pressed """
+    #TODO perhaps this should be removed
     return button_b.was_pressed()
 
 def clear_extra_space():
+    """ remove extra space at list front """
     global message
     if len(message) == 1 and message[0] == SPACE:
         message.clear()
 
 def display_dot_dash():
+    """ based on time interval, display appropriate image """
     mark_interval = running_time() - mark_start_time
 
     if mark_interval < DOT_TIME_MAX:
@@ -74,6 +99,7 @@ def display_dot_dash():
         display.show(OVER_TIME_IMAGE)
 
 def process_mark_time():
+    """ Determine if DOT or DASH timing has elapsed """
     global mark_has_begun, mark_start_time
     if not mark_has_begun: return
 
@@ -91,9 +117,11 @@ def process_mark_time():
     mark_has_begun = False
     
 def message_has_first_mark():
+    """ Message list contains at lease one dot or dash """
     return len(message) > 0 and message[0] != SPACE
 
 def process_space_time():
+    """ Determines if enough time has elapsed to generate a space """
     global space_has_begun, space_start_time, mark_start_time
     if not message_has_first_mark(): return
 
@@ -111,6 +139,7 @@ def process_space_time():
     space_has_begun = False
 
 def encode_keyed_morse_code():
+    """ Depending on button state, encode keys to marks """
     global mark_has_begun, mark_start_time, message, space_start_time, space_has_begun
 
     if clear_command_received():
@@ -139,6 +168,7 @@ def encode_keyed_morse_code():
             process_mark_time()
 
 def display_character(character):
+    """ Display Dot, Dash, or Space images on LED Matrix """
     if character == DASH:
         display.show(DASH_IMAGE)
         music.pitch(TONE_PITCH,duration=int(round(DASH_TIME)),wait=True)
@@ -157,12 +187,7 @@ def display_character(character):
 
 
 def process_incoming_serial_data():
-    """
-        gets comma delimited data from serial
-        applies changes appropriately
-
-        returns true if reset received
-    """
+    """ gets comma delimited data from serial applies changes appropriately """
     global message
     built_string = ""
     while uart.any() is True:
@@ -176,7 +201,7 @@ def process_incoming_serial_data():
         if built_string != "":
             parsed_data = built_string.split(",")
             try:
-                serial_mode_str = parsed_data[0]
+                serial_mode_str = parsed_data[0] # TODO do we need to do anything with serial_mode??
                 serial_message_str = parsed_data[1]
                 clear_message_str = parsed_data[2]
             except IndexError:
@@ -184,20 +209,20 @@ def process_incoming_serial_data():
             
             if clear_message_str:
                 message.clear()
-
-            # TODO do we need to do anything with serial_mode??
+        
             if serial_message_str and len(serial_message_str) > 0:
                 for character in serial_message_str:
                     display_character(character)
 
 uart.init(baudrate=9600) 
-uart.write(" , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ,"+EOL)
+uart.write(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"+EOL)
 last_message_length = 0
 
 while True:
     encode_keyed_morse_code()
     process_incoming_serial_data()
     if last_message_length != len(message):
-        uart.write("Morse{}".format(message)+EOL)         
+        for character in message:
+            uart.write("{},".format(character))
+        uart.write(EOL)         
         last_message_length = len(message)
-
